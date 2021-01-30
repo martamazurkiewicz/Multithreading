76f57044b053e16f0e9fe0fa691934dac59f77d2
martamazurkiewicz <marta@mazurkiewi.cz>
2021-01-30 17:19:57 +0100

    Deleted ref from Update.CAS

--- Task3/Update.cs
+++ Task3/Update.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using System.Threading;
+﻿using System.Threading;
 
 namespace Task3
 {
@@ -57,29 +55,7 @@ namespace Task3
             this.info = info;
             semaphore = new SemaphoreSlim(1,1);
         }
-
-        // public static Update CAS(ref Update CASObject, Update oldValue, Update newValue)
-        // {
-        //     //If R is a CAS object, then CAS(R, old, new) changes the value of
-        //     //R to new if the object’s value was old, in which case we say the CAS was successful.
-        //     //CAS always returns the value the object had prior to the operation.
-        //     //How CAS know how to compare Update (implement IComparable ?)
-        //     var result = Interlocked.CompareExchange<Update>(ref CASObject, newValue, oldValue);
-        //     return result;
-        // }
-
-        // public static Update CAS(ref Update CASObject, Update comparand, Update newValue)
-        // {
-        //     Update oldValue;
-        //     do
-        //     {
-        //         oldValue = CASObject;
-        //         if (!oldValue.Equals(comparand)) return oldValue;
-        //     } while (Interlocked.CompareExchange(ref CASObject, newValue, comparand) != oldValue);
-        //     return oldValue;
-        // }
-        
-        public static Update CAS(ref Update CASObject, Update comparator, Update newValue)
+        public static Update CAS(Update CASObject, Update comparator, Update newValue)
         {
             var oldValue = CASObject;
             if (CASObject.semaphore.CurrentCount == 1)
@@ -91,13 +67,10 @@ namespace Task3
             }
             return oldValue;
         }
-
         public override string ToString() => info == null ? state + " null" : state + " " + info;
-
-         public override bool Equals(object obj) =>
+        public override bool Equals(object obj) =>
              state == ((Update) obj).state && info == ((Update) obj).info;
-        
-         public static bool operator ==(Update update1, Update update2) => update1.Equals(update2);
+        public static bool operator ==(Update update1, Update update2) => update1.Equals(update2);
          public static bool operator !=(Update update1, Update update2) => !(update1 == update2);
     }
 }
